5c82117212a72db6e51b961ab540d94e82bea045
Chloe <cehan16@gmail.com>
2016-06-25 22:18:38 +0800

    Q74

---
+++ 74FirstBadVersion.py
@@ -0,0 +1,24 @@
+#classSVNRepo:
+#    @classmethod
+#    def isBadVersion(cls, id)
+#        # Run unit tests to check whether verison `id` is a bad version
+#        # return true if unit tests passed else false.
+# You can useSVNRepo.isBadVersion(10) to check whether version 10 is a 
+# bad version.
+class Solution:
+    """
+    @param n: An integers.
+    @return: An integer which is the first bad version.
+    """
+    def findFirstBadVersion(self, n):
+        start = 1
+        end = n
+        while(start + 1 < end):
+            mid = start + (end - start) / 2
+            if SVNRepo.isBadVersion(mid):
+                end = mid
+            else:
+                start = mid
+        if SVNRepo.isBadVersion(start):
+            return start
+        return end
