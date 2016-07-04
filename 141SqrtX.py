30e0afdf91f9ee8778344ca18335cc7fc3398c17
Chloe <cehan16@gmail.com>
2016-07-04 11:10:32 +0800

    Q141

--- 141SqrtX.py
+++ 141SqrtX.py
@@ -4,7 +4,19 @@ class Solution:
     @return: The sqrt of x
     """
     def sqrt(self, x):
-        # write your code here
         if x < 0:
-            return None
-        return int(math.floor(x ** 0.5))
+            return -1
+        start = 0
+        end = x
+        while start + 1 < end:
+            mid = start + (end - start) / 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                start = mid
+            else:
+                end = mid
+        if end * end <= x:
+            return end
+        return start
+
