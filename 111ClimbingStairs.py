21fd285b3abd0d50685d4d137f83985307cb2c72
Chloe <cehan16@gmail.com>
2016-07-27 18:26:34 +0800

    Q111

---
+++ 111ClimbingStairs.py
@@ -0,0 +1,21 @@
+class Solution:
+    """
+    @param n: An integer
+    @return: An integer
+    """
+    def climbStairs(self, n):
+        f = [[] for i in range(n + 1)]
+        # now i still don't know why f[0] should be 1
+        if i == 0:
+            return 1
+        if i == 1:
+            return 1
+        if i == 2:
+            return 2
+        f[1] = 1
+        f[2] = 2
+        for i in range(3, n+1):
+            f[i] = f[i-1] + f[i-2] 
+        return f[n]
+        
+        
